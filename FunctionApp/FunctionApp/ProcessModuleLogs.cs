0f2ef9d3f26a74888dafd69319d5bcc00754ac2e
Marvin Garcia <magar@microsoft.com>
2021-03-22 18:08:20 -0400

    filter log by log level when reading blob

--- FunctionApp/FunctionApp/ProcessModuleLogs.cs
+++ FunctionApp/FunctionApp/ProcessModuleLogs.cs
@@ -2,11 +2,10 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 using FunctionApp.Models;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace FunctionApp
 {
@@ -15,6 +14,7 @@ namespace FunctionApp
         private static string _workspaceId = Environment.GetEnvironmentVariable("WorkspaceId");
         private static string _workspaceKey = Environment.GetEnvironmentVariable("WorkspaceKey");
         private static string _workspaceApiVersion = Environment.GetEnvironmentVariable("WorkspaceApiVersion");
+        private static int _logsLogLevel = Convert.ToInt32(Environment.GetEnvironmentVariable("LogsLogLevel"));
         private static string _logType = Environment.GetEnvironmentVariable("LogType");
         private static int _logMaxSizeMB = Convert.ToInt32(Environment.GetEnvironmentVariable("LogsMaxSizeMB"));
 
@@ -28,9 +28,15 @@ namespace FunctionApp
             {
                 log.LogInformation($"ProcessModuleLogs Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
                 
-                // convert logs to intended format class
+                // Cast logs to original format
                 StreamReader reader = new StreamReader(myBlob);
                 IoTEdgeLog[] iotEdgeLogs = JsonConvert.DeserializeObject<IoTEdgeLog[]>(reader.ReadToEnd());
+
+                // Filter logs by log level
+                // This won't be necessary once the direct method filters logs successfully
+                iotEdgeLogs = iotEdgeLogs.Where(x => x.LogLevel <= _logsLogLevel).ToArray();
+
+                // Convert to logs their final log analytics format
                 LogAnalyticsLog[] logAnalyticsLogs = iotEdgeLogs.Select(x => new LogAnalyticsLog(x)).ToArray();
 
                 if (logAnalyticsLogs.Length == 0)
@@ -47,7 +53,7 @@ namespace FunctionApp
                 // we have to break logs in chunks to fit in on each request
                 byte[] logBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(logAnalyticsLogs));
                 double chunks = Math.Ceiling(logBytes.Length / (_logMaxSizeMB * 1024f * 1024f));
-                
+
                 // get right number of items for the logs array
                 int steps = Convert.ToInt32(Math.Ceiling(logAnalyticsLogs.Length / chunks));
 
@@ -56,10 +62,18 @@ namespace FunctionApp
                 {
                     int limit = count + steps < logAnalyticsLogs.Length ? count + steps: logAnalyticsLogs.Length;
 
-                    log.LogInformation($"Writing logs {count} - {limit} / {logAnalyticsLogs.Length} to Log Analytics Workspace");
+                    log.LogInformation($"Submitting dtaa collection request for logs {count + 1} - {limit} / {logAnalyticsLogs.Length}");
 
                     LogAnalyticsLog[] logsChunk = logAnalyticsLogs.Skip(count).Take(limit).ToArray();
-                    logAnalytics.Post(JsonConvert.SerializeObject(logsChunk));
+                    try
+                    {
+                        logAnalytics.Post(JsonConvert.SerializeObject(logsChunk));
+                        log.LogInformation("Request successful");
+                    }
+                    catch (Exception e)
+                    {
+                        log.LogError($"Request failed with exception {e}");
+                    }
 
                     count += steps;
                 }
@@ -68,7 +82,6 @@ namespace FunctionApp
             catch (Exception e)
             {
                 log.LogError($"GetModuleLogs failed with the following exception: {e}");
-                throw e;
             }
         }
     }
