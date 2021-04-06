efe290485d928bb06c4d9ce6e0438613f696537b
Marvin Garcia <magar@microsoft.com>
2021-04-06 13:00:29 -0400

    updated Ubuntu version to 18.04

--- Scripts/edge-setup.sh
+++ Scripts/edge-setup.sh
@@ -16,18 +16,18 @@ echo "Prepare machine..."
 DEBIAN_FRONTEND=noninteractive
 
 # install powershell and iotedge
-curl -O https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+curl -O https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get update
 apt-get install -y --no-install-recommends powershell
 echo "Powershell installed."
 
-curl -o microsoft-prod.list https://packages.microsoft.com/config/ubuntu/16.04/multiarch/prod.list
+curl -o microsoft-prod.list https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list
 cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 apt-get update
-apt-get install -y --no-install-recommends moby-engine moby-cli
+apt-get install -y --no-install-recommends moby-engine moby-cli libiothsm-std
 apt-get install -y --no-install-recommends iotedge
 echo "Iotedge installed."
 
